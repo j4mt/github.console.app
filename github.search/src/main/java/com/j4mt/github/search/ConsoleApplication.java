f4cc93352c95229f47f86be6091cae554542ed1e
j4mt <johm.orourke.1985@gmail.com>
2019-12-15 20:29:29 +0000

    Adding basic search service - search user - RestTemplate code

--- github.search/src/main/java/com/j4mt/github/search/ConsoleApplication.java
+++ github.search/src/main/java/com/j4mt/github/search/ConsoleApplication.java
@@ -1,36 +1,50 @@
 package com.j4mt.github.search;
 
 import com.j4mt.github.search.service.HelloService;
+import com.j4mt.github.search.service.SearchService;
 import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootApplication
 public class ConsoleApplication implements CommandLineRunner {
 
-	private HelloService helloService;
+    private HelloService helloService;
 
-	public ConsoleApplication(HelloService helloService){
-		this.helloService = helloService;
-	}
+    private SearchService searchService;
 
-	public static void main(String[] args) {
+    public ConsoleApplication(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    public static void main(String[] args) {
 //		SpringApplication.run(ConsoleApplication.class, args);
-		SpringApplication app = new SpringApplication(ConsoleApplication.class);
-		// disable spring banner
-		app.setBannerMode(Banner.Mode.OFF);
-		app.run(args);
-	}
+        SpringApplication app = new SpringApplication(ConsoleApplication.class);
+//         disable spring banner
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
 
-	@Override
-	public void run(String... args) throws Exception {
-		// check if the user passes any arguments
-		if(args.length > 0){
-			System.out.println(helloService.getMessage(args[0]));
-		} else {
-			// print default message
-			System.out.println(helloService.getMessage());
-		}
-	}
+    @Override
+    public void run(String... args) throws Exception {
+//         check if the user passes any arguments
+        if (args.length > 0) {
+            if (args[0].equalsIgnoreCase("searchUser")) {
+                System.out.println("In search user");
+                ResponseEntity<String> response = searchService.searchUser(args[1]);
+                //TODO: display User information in Console
+            } else if (args[0].equalsIgnoreCase("searchRepo")) {
+                System.out.println("In search Repo");
+//				System.out.println(searchService.searchRepo(args[1]));
+            } else {
+                System.out.println(helloService.getMessage(args[0]));
+            }
+
+        } else {
+            // print default message
+            System.out.println(helloService.getMessage());
+        }
+    }
 }
--- github.search/src/main/java/com/j4mt/github/search/service/SearchService.java
+++ github.search/src/main/java/com/j4mt/github/search/service/SearchService.java
@@ -1,4 +1,37 @@
 package com.j4mt.github.search.service;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+
+@Service
 public class SearchService {
+
+    @Value("${github.respository.uri}")
+    private String uri;
+
+    private String searchCriteria;
+
+    public ResponseEntity<String> searchUser(String user) {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
+        ResponseEntity<String> result = restTemplate.exchange(uri + "/users/" + user, HttpMethod.GET, entity, String.class);
+
+        System.out.println(result);
+        return result;
+    }
+
+    public String searchRepo(String repo) {
+        //TODO: add restful search call here for user
+        return "NOR IMPLEMENTED :  restful search call for Repository Search : " + repo;
+    }
+
 }
